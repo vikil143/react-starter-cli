00fcdccf29e424e7f596937d618b8adbc3b49733
vikil143 <vikil.lakkavatri25@gmail.com>
2026-03-25 16:40:25 +0530

    installation of project generation

--- src/generators/projectGenerator.js
+++ src/generators/projectGenerator.js
@@ -1,6 +1,6 @@
 import { buildJavaScriptTemplate } from "../templates/js/index.js";
 import { buildTypeScriptTemplate } from "../templates/ts/index.js";
-import { runCommand } from "../utils/exec.js";
+import { runCommand, runCommandAsPromise } from "../utils/exec.js";
 import { ensureDirectories, removePaths, verifyFiles, writeFiles } from "../utils/file.js";
 import { logInfo, logStep } from "../utils/logger.js";
 import { updatePackageJson } from "../utils/packageJson.js";
@@ -11,7 +11,7 @@ export async function createProject({ projectName, projectPath, template, featur
   const generated = builder({ features, components });
 
   logStep("Scaffolding the Vite app...");
-  runCommand(`npm create vite@latest --yes ${projectName} -- --template ${viteTemplate}`, { stdio: "inherit" });
+  await runCommandAsPromise(`npm create vite@latest --yes ${projectName} -- --template ${viteTemplate}`, { stdio: "inherit" });
 
   logStep("Writing starter files...");
   removePaths(projectPath, [
--- src/utils/exec.js
+++ src/utils/exec.js
@@ -1,4 +1,4 @@
-import { execSync } from "node:child_process";
+import { exec, execSync } from "node:child_process";
 
 export function runCommand(command, options = {}) {
   const { env, ...restOptions } = options;
@@ -13,6 +13,36 @@ export function runCommand(command, options = {}) {
   });
 }
 
-export async function runCommandAsync(command, options = {}) {
-  return runCommand(command, options);
+export function runCommandAsPromise(command, options = {}) {
+  const { env, ...restOptions } = options;
+
+  return new Promise((resolve, reject) => {
+    const child = exec(command, {
+      ...restOptions,
+      env: {
+        ...process.env,
+        ...(env || {}),
+      },
+    });
+
+    console.log(`Running command: ${command}`);
+    if (child.stdout) {
+      child.stdout.pipe(process.stdout);
+    }
+
+    if (child.stderr) {
+      child.stderr.pipe(process.stderr);
+    }
+
+    child.on("error", reject);
+
+    child.on("close", (code) => {
+      if (code === 0) {
+        resolve();
+        return;
+      }
+
+      reject(new Error(`Command failed with exit code ${code}: ${command}`));
+    });
+  });
 }
